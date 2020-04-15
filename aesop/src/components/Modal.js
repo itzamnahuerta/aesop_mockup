fdace15449b0674ee86a52647130458f7504d057
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-14 20:12:40 -0400

    started making modal

---
+++ aesop/src/components/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+{/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_bottom */}
+function Modal() {
+  return (
+    <div className="modal">
+      some text outside
+      <div className="modalSideBar">
+        some text inside
+        <span className="thex">&times;</span>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
