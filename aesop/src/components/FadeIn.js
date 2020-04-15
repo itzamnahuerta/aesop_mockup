b84f0d627045888e51b7c3562a916b934464d291
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-15 18:30:36 -0400

    made FadeIn

---
+++ aesop/src/components/FadeIn.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class FadeIn extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    componentDidMount(){
+        
+    }
+    render() {
+        return (
+            <div>
+                FadeIn
+            </div>
+          );
+
+    }
+  
+}
+
+export default FadeIn;
