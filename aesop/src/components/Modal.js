098f50444b966e71d5c92329430c2dae3b8ad73f
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-14 20:43:52 -0400

    turned Modal into a class

--- aesop/src/components/Modal.js
+++ aesop/src/components/Modal.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 {/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_bottom */}
-function Modal() {
-  return (
-    <div className="modal">
-      <div className="modalSideBar">
-        
-        <span className="thex">&times;</span>
-        some text inside
-      </div>
-    </div>
-  );
+class Modal extends Component {
+    render() {
+        return (
+            <div className="modal">
+              <div className="modalSideBar">
+                
+                <span className="thex">&times;</span>
+                some text inside
+              </div>
+            </div>
+          );
+
+    }
+  
 }
 
 export default Modal;
