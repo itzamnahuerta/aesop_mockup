c99a1812c621575797f297370326b5888b901f05
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-14 21:11:56 -0400

    added modal functions

--- aesop/src/components/Modal.js
+++ aesop/src/components/Modal.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 {/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_bottom */}
 class Modal extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    componentDidMount(){
+        let modal = document.getElementsByClassName("modal")[0];
+        let thex = document.getElementsByClassName("thex")[0];
+        let link = document.getElementById("hoverThis");
+        link.onclick = function() {
+            modal.style.display = "block";
+          };
+        thex.onclick = function() {
+            modal.style.display = "none";
+          };
+    }
     render() {
         return (
             <div className="modal">
