82e5b2514a216f6de88b07dfcd26ab99a209b484
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-14 23:25:48 -0400

    got buttons to slide in and out

--- aesop/src/components/Scrollbar.js
+++ aesop/src/components/Scrollbar.js
@@ -4,21 +4,73 @@ import React, { Component } from 'react';
 class Scrollbar extends Component {
     constructor(props) {
         super(props);
-        
+        this.checkScrollBar = this.checkScrollBar.bind(this);
     }
     componentDidMount() {
         let scrollBar = document.getElementsByClassName('scrollBar')[0];
         let leftButton = document.getElementsByClassName("LeftButton")[0];
         let rightButton = document.getElementsByClassName("RightButton")[0];
+        console.log(leftButton.style.animation);
+        console.log(rightButton.style.animation);
+        if(scrollBar.scrollLeft===0){
+            console.log(leftButton.animate);
+            leftButton.style.left="-200px"
+            //setTimeout(leftButton.style.display = "none",1000);
+        } else if (scrollBar.scrollLeft>=window.outerWidth){
+            rightButton.style.right="-200px";
+            //setTimeout(rightButton.style.display = "none",1000);
+        } else {
+            leftButton.style.left="0";
+            rightButton.style.right="0";
+        }
         leftButton.onclick = function() {
             scrollBar.scrollLeft-=200;
+            if(scrollBar.scrollLeft===0){
+                console.log(leftButton.animate);
+                leftButton.style.left="-200px"
+                //setTimeout(leftButton.style.display = "none",1000);
+            } else if (scrollBar.scrollLeft>=window.outerWidth){
+                rightButton.style.right="-200px";
+                //setTimeout(rightButton.style.display = "none",1000);
+            } else {
+                leftButton.style.left="0"
+                leftButton.animate="slideIn";
+                rightButton.style.right="0";
+                rightButton.animate="slideInRight";
+            }
+            //this.checkScrollBar(scrollBar,leftButton,rightButton);
           };
         rightButton.onclick = function() {
             scrollBar.scrollLeft+=200;
+            console.log(scrollBar.scrollLeft);
+            if(scrollBar.scrollLeft===0){
+                console.log(leftButton.animate);
+                leftButton.style.left="-200px"
+                //setTimeout(leftButton.style.display = "none",1000);
+            } else if (scrollBar.scrollLeft>=window.outerWidth){
+                rightButton.style.right="-200px";
+                //setTimeout(rightButton.style.display = "none",1000);
+            } else {
+                leftButton.style.left="0"
+                leftButton.animate="slideIn";
+                rightButton.style.right="0";
+                rightButton.animate="slideInRight";
+            }
+            //this.checkScrollBar(scrollBar,leftButton,rightButton);
         };
         
     }
     
+    checkScrollBar(element,left,right) {
+        if(element.scrollLeft===0){
+            left.style.display = "none";
+        } else if (element.scrollLeft>=window.outerWidth){
+            right.style.display = "none";
+        } else {
+            left.style.display = "block";
+            right.style.display = "block";
+        }
+    }
     render() {
         return (
             <div>
