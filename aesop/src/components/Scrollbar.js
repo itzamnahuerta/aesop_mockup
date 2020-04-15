5ab1523530005b47f94316ca4be61ab684625d58
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-14 22:35:56 -0400

    styled and positioned buttons

--- aesop/src/components/Scrollbar.js
+++ aesop/src/components/Scrollbar.js
@@ -23,14 +23,16 @@ class Scrollbar extends Component {
         return (
             <div>
                 <div className="scrollBar">
+                <button className="LeftButton">Left Arrow</button>
                     <img src={this.props.ImageOne} height="400px"/>
                     <img src={this.props.ImageTwo} height="400px" />
                     <img src={this.props.ImageThree} height="400px"/>
                     <img src={this.props.ImageFour} height="400px"/>
                     <img src={this.props.ImageFive} height="400px"/>
+                    <button className="RightButton">Right Arrow</button>
                 </div>
-                <button className="LeftButton">Left Arrow</button>
-                <button className="RightButton">Right Arrow</button>
+                
+                
             </div>
           );
 
