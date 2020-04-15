a3c85b193c76a40c862c8bc0bf18ce821068b9b6
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-14 22:12:33 -0400

    made left and right arrow

--- aesop/src/components/Scrollbar.js
+++ aesop/src/components/Scrollbar.js
@@ -8,12 +8,16 @@ class Scrollbar extends Component {
     
     render() {
         return (
-            <div className="scrollBar">
-                <img src={this.props.ImageOne} height="400px"/>
-                <img src={this.props.ImageTwo} height="400px" />
-                <img src={this.props.ImageThree} height="400px"/>
-                <img src={this.props.ImageFour} height="400px"/>
-                <img src={this.props.ImageFive} height="400px"/>
+            <div>
+                <div className="scrollBar">
+                    <img src={this.props.ImageOne} height="400px"/>
+                    <img src={this.props.ImageTwo} height="400px" />
+                    <img src={this.props.ImageThree} height="400px"/>
+                    <img src={this.props.ImageFour} height="400px"/>
+                    <img src={this.props.ImageFive} height="400px"/>
+                </div>
+                <button>Left Arrow</button>
+                <button>Right Arrow</button>
             </div>
           );
 
